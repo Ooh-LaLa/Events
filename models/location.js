05da916b27eea1882b14623ea6a3fe64a346ae3f
ooh-lala <laurentozzi.code@gmail.com>
2023-01-20 14:58:13 -0500

    update model

--- models/location.js
+++ models/location.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const locationSchema = new Schema ({ 
-  name: { type: String },
-  address: { type: String },
+  name: { type: String, required: true },
+  address: { type: String, required: true },
   owner: {type: Schema.Types.ObjectId, ref: "Profile"}
 }, {
   timestamps: true})
